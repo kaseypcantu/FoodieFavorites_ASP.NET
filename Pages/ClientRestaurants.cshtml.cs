6b8c20c7e9314227e95aa6c13c2b4a3f06399e79
kaseypcantu <kaseypaulcantu@gmail.com>
2020-04-07 18:45:55 -0500

    Implementing client-side JS to interact with the backend C# API

---
+++ Pages/ClientRestaurants.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodieFavorites.Pages
+{
+    public class ClientRestaurants : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
